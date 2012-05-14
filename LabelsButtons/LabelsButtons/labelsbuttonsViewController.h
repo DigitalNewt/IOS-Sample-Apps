7ec72c78ef22b156fbaaa3eb55ab26ea1d831bb3
Brent Baker <Brent.Baker@dccusa.com>
2012-05-14 09:24:25 -0500

    IOS Development 8

---
+++ LabelsButtons/LabelsButtons/labelsbuttonsViewController.h
@@ -0,0 +1,14 @@
+//
+//  labelsbuttonsViewController.h
+//  LabelsButtons
+//
+//  Created by Baker Brent on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface labelsbuttonsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
+@end
---
+++ LabelsButtons/LabelsButtons/labelsbuttonsViewController.m
@@ -0,0 +1,67 @@
+//
+//  labelsbuttonsViewController.m
+//  LabelsButtons
+//
+//  Created by Baker Brent on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "labelsbuttonsViewController.h"
+
+@implementation labelsbuttonsViewController
+@synthesize myLabel;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (IBAction)ButtonPressed:(id)sender {
+    myLabel.text = @" This is a sentence ";
+
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setMyLabel:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
