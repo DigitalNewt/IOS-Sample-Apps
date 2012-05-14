fbf8e796dd77d8b19206498a5428c82cb8499f98
Brent Baker <Brent.Baker@dccusa.com>
2012-05-14 18:10:25 -0500

    IOS 5 Development 13 tutorial

---
+++ sliderGame/sliderGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sliderGame
+//
+//  Created by Baker Brent on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "sliderGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([sliderGameAppDelegate class]));
+    }
+}
---
+++ sliderGame/sliderGameTests/sliderGameTests.m
@@ -0,0 +1,32 @@
+//
+//  sliderGameTests.m
+//  sliderGameTests
+//
+//  Created by Baker Brent on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "sliderGameTests.h"
+
+@implementation sliderGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in sliderGameTests");
+}
+
+@end
