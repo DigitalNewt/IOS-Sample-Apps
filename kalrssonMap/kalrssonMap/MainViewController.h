6536b556afba173b43455a6fdd51c09798d7cb7e
Brent Baker <Brent.Baker@dccusa.com>
2012-05-19 13:18:28 -0500

    IOS 5 Development 22 Tutorial

---
+++ kalrssonMap/kalrssonMap/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  kalrssonMap
+//
+//  Created by Baker Brent on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ kalrssonMap/kalrssonMapTests/kalrssonMapTests.m
@@ -0,0 +1,32 @@
+//
+//  kalrssonMapTests.m
+//  kalrssonMapTests
+//
+//  Created by Baker Brent on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "kalrssonMapTests.h"
+
+@implementation kalrssonMapTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in kalrssonMapTests");
+}
+
+@end
