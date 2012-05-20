b4a43c3a35588df22c3f50d995f24871cd1bf93e
Brent Baker <Brent.Baker@dccusa.com>
2012-05-20 15:32:38 -0500

    iOS 5 Development 31 Tutorial

---
+++ mk/mkTests/mkTests.h
@@ -0,0 +1,13 @@
+//
+//  mkTests.h
+//  mkTests
+//
+//  Created by Baker Brent on 5/20/12.
+//  Copyright (c) 2012 Global Institution. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface mkTests : SenTestCase
+
+@end
