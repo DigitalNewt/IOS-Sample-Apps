9d382c9c577aacb8b7ca5b75e341180e49aff3a1
Brent Baker <Brent.Baker@dccusa.com>
2012-05-20 10:35:46 -0500

    IOS 5 Development 24 Tutorial

---
+++ TapProject/TapProjectTests/TapProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  TapProjectTests.m
+//  TapProjectTests
+//
+//  Created by Baker Brent on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TapProjectTests.h"
+
+@implementation TapProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TapProjectTests");
+}
+
+@end
