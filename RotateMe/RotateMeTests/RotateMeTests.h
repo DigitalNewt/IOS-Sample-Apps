52bfccd9b1d22194725f0186780e54b9bb635e99
Brent Baker <Brent.Baker@dccusa.com>
2012-05-20 11:33:04 -0500

    IOS 5 Development 27 Tutorial

---
+++ RotateMe/RotateMeTests/RotateMeTests.h
@@ -0,0 +1,13 @@
+//
+//  RotateMeTests.h
+//  RotateMeTests
+//
+//  Created by Baker Brent on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RotateMeTests : SenTestCase
+
+@end
