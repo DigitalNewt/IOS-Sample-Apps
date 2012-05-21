11385c654ca1fcc10702c735c6d530dd41061d86
Brent Baker <Brent.Baker@dccusa.com>
2012-05-20 22:12:37 -0500

    iOS 5 Development 34 Tutorial

---
+++ ProgressView/ProgressView/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  ProgressView
+//
+//  Created by Baker Brent on 5/20/12.
+//  Copyright (c) 2012 Global Institution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
