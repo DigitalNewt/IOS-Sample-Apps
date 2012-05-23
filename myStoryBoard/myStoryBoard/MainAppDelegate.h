44151a9e60d1992b4942f7a6db409d5a86197eba
Brent Baker <Brent.Baker@dccusa.com>
2012-05-22 22:06:00 -0500

    iOS 5 Development 36 Tutorial

---
+++ myStoryBoard/myStoryBoard/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  myStoryBoard
+//
+//  Created by Baker Brent on 5/22/12.
+//  Copyright (c) 2012 Global Institution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ myStoryBoard/myStoryBoardTests/myStoryBoardTests.m
@@ -0,0 +1,32 @@
+//
+//  myStoryBoardTests.m
+//  myStoryBoardTests
+//
+//  Created by Baker Brent on 5/22/12.
+//  Copyright (c) 2012 Global Institution. All rights reserved.
+//
+
+#import "myStoryBoardTests.h"
+
+@implementation myStoryBoardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in myStoryBoardTests");
+}
+
+@end
