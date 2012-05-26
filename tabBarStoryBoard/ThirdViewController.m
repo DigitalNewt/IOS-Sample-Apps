1dcbe9821472820ceaf06b949a5083a9af155ce0
Brent Baker <Brent.Baker@dccusa.com>
2012-05-26 06:59:42 -0500

    iOS 5 Development 38 Tutorial

---
+++ tabBarStoryBoard/ThirdViewController.m
@@ -0,0 +1,19 @@
+//
+//  ThirdViewController.m
+//  tabBarStoryBoard
+//
+//  Created by Baker Brent on 5/26/12.
+//  Copyright (c) 2012 Global Institution. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@implementation ThirdViewController
+@synthesize myLabel;
+
+- (IBAction)buttonPressed:(id)sender {
+    myLabel.text = @"OK";
+}
+
+
+@end
